ce33ad97044dae47269762c7f75a9f75ec4b3973
jmni-cn <1711008052@qq.com>
2024-11-19 16:41:07 +0800

    chore: commitlint兼容semantic-release

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-}
+  rules: {
+    // 放宽对 `chore(release):` 提交类型的限制
+    'scope-enum': [2, 'always', ['release']],
+    'scope-case': [0], // 允许 `scope` 小写
+    'subject-case': [0], // 允许 `subject` 中的大小写
+  },
+  ignores: [
+    (commit) => commit.includes('[skip ci]'),
+  ],
+};
